a7dfc7f0231295bbfb563ce5f61e82bed221c627
TimeSmash <TimeSmash@github.com>
2019-06-24 19:11:32 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < Sinatra::Base
 
-  configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
+  configure do   #tells controller where to look for views (.erb files)
+  	set :views, "app/views" #look here for views
+  	set :public_dir, "public" #Directory that all users can access
   end
 
   get "/" do
   	erb :index
   end
+
+  get '/dogs' do
+    erb :dogs
+end
 end
